cbf223888e494a075cb7d2e87f2b1ccf31d16521
CeciliaCavallini <caval.ceci@gmail.com>
2020-07-03 19:58:22 +0200

    Add cards

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -4,11 +4,13 @@ import React from 'react';
 // == Import
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
+import Team from 'src/components/Team';
 
 // == Composant
 const App = () => (
   <div className="app">
     <Header />
+    <Team />
     <Footer />
   </div>
 );
