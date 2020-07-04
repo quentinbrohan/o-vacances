2d182416adb8bfc740650db9341ddad45c82d8e0
QuentinBrohan <brohan.quentin@gmail.com>
2020-07-05 00:55:24 +0200

    :lipstick: Fix Footer alignment

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -4,7 +4,6 @@ import React from 'react';
 // == Import
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
-import Team from 'src/components/Team';
 
 import './styles.scss';
 
@@ -13,7 +12,6 @@ const App = () => (
   <div className="app">
     <Header />
     <div className="container">
-      <Team />
     </div>
     <Footer />
   </div>
