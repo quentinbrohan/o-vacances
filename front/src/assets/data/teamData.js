6ac451bdf4608e56259b499380e2e26eb9e76a28
CeciliaCavallini <caval.ceci@gmail.com>
2020-07-04 18:51:07 +0200

    :card_file_box: add datas for team cards

---
+++ front/src/assets/data/teamData.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+export default {
+  "team":[
+    { "name": "Quentin Brohan",
+    "linkedin": "https://www.linkedin.com/in/quentinbrohan/",
+    "github": "https://github.com/quentinbrohan",
+    "portfolio": "http://quentinbrohan.github.io/",
+    "autre":"Infographiste",
+  },
+    { "name": "Cavallini Cécilia",
+    "linkedin": "https://www.linkedin.com/in/ceciliacavallini/",
+    "github": "https://github.com/CeciliaCavallini/Public",
+    "portfolio": " ",
+    "autre":"Sommellerie",
+  },
+
+
+
+]};
