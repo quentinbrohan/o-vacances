2f47fbe475deca3988a07933ff96a24064c33119
QuentinBrohan <brohan.quentin@gmail.com>
2020-07-04 20:01:06 +0200

    :sparkles: Add Header. :heavy_plus_sign: add react-feather react-burger-menu

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
+import { slide as Menu } from 'react-burger-menu';
+import { Menu as MenuIcon, X as CloseIcon } from 'react-feather';
 
 import './header.scss';
 
@@ -8,14 +8,38 @@ const Header = () => (
   <header>
     <a href="/">LOGO</a>
     <nav className="navigation">
-      <ul>
-        <li><a href="">Mes voyages</a></li>
-        <li><a href="">Créer un voyage</a></li>
-        <li><a href="">Mon profil</a></li>
-      </ul>
-      <div className="connection">
-        <Button>Se connecter</Button>
-        <Button>Sinscrire</Button>
+      <div className="mobile">
+        <Menu
+          right
+          noOverlay
+          className="bm-menu"
+          customBurgerIcon={<MenuIcon />}
+          customCrossIcon={<CloseIcon />}
+        >
+          <a href="">Mes voyages</a>
+          <a href="">Créer un voyage</a>
+          <a href="">Mon profil</a>
+
+          <div className="connection">
+            <button type="button">
+              <a href="/">Connexion</a>
+            </button>
+            <button type="button">
+              <a href="/">Déconnexion</a>
+            </button>
+          </div>
+        </Menu>
+      </div>
+      <div className="menu">
+        <ul>
+          <li><a href="">Mes voyages</a></li>
+          <li><a href="">Créer un voyage</a></li>
+          <li><a href="">Mon profil</a></li>
+        </ul>
+        <div className="connection">
+          {/* <Button>Se connecter</Button> */}
+          {/* <Button>Sinscrire</Button> */}
+        </div>
       </div>
     </nav>
   </header>
