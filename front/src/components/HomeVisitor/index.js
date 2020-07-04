bdcdcb1543b6f612d07e7a2e7f419c8f07803acd
QuentinBrohan <brohan.quentin@gmail.com>
2020-07-05 01:41:02 +0200

    :construction Create data > homevisitor for HomeVisitor slideshow

---
+++ front/src/components/HomeVisitor/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import slideshowData from 'src/data/homeVisitorData';
+
+const HomeVisitor = () => {
+  console.log(slideshowData);
+
+  return (
+    <div className="home-visitor">
+      Home
+    </div>
+  );
+};
+
+export default HomeVisitor;
---
+++ front/src/data/homeVisitorData.js
@@ -0,0 +1,29 @@
+// Contains our homepage visitor's data for slideshow
+// Using https://unsplash.com/ till we choose photos
+
+export default [
+  {
+    name: 'à la campagne',
+    image: 'https://images.unsplash.com/photo-1517792844039-e52afb564132?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2129&q=80',
+  },
+  {
+    name: 'à la mer',
+    image: 'https://images.unsplash.com/photo-1525183995014-bd94c0750cd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  },
+  {
+    name: 'à la montagne',
+    image: 'https://images.unsplash.com/photo-1517021897933-0e0319cfbc28?ixlib=rb-1.2.1&auto=format&fit=crop&w=2038&q=80',
+  },
+  {
+    name: 'à l\'étranger',
+    image: 'https://images.unsplash.com/photo-1532724028424-8a45f61658dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80',
+  },
+  {
+    name: 'en camping',
+    image: 'https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  },
+  {
+    name: 'sportives',
+    image: 'https://images.unsplash.com/photo-1551632811-561732d1e306?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  },
+];
