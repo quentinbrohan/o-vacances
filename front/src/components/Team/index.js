1d8ec858761168e2a81569b7d67acef83e29b3e6
CeciliaCavallini <caval.ceci@gmail.com>
2020-07-04 20:06:51 +0200

    add card in team page

--- front/src/components/Team/index.js
+++ front/src/components/Team/index.js
@@ -1,23 +1,38 @@
 import React from 'react';
 
+import {
+  GitHub,
+  Linkedin,
+  Link,
+  UserPlus,
+} from 'react-feather';
 import './Team.scss';
 
 const Team = () => (
-
-  <div className="team-cards">
-
-    <ul>
-      <li>
-
-        <a href="#">Linkedin</a>
-      </li>
-      <li>
-
-        <a href="#">Github</a>
-      </li>
-    </ul>
-
-  </div>
+  <article className="team-cards">
+    <div className="team-card">
+      <img src="" alt="" />
+      <h2>Nom Prénom</h2>
+      <ul>
+        <li>
+          <Linkedin />
+          <a href="#">Linkedin</a>
+        </li>
+        <li>
+          <GitHub />
+          <a href="#">Github</a>
+        </li>
+        <li>
+          <Link />
+          <a href="#">Lien portfolio</a>
+        </li>
+        <li>
+          <UserPlus />
+          <a href="#">Autre</a>
+        </li>
+      </ul>
+    </div>
+  </article>
 );
 
 export default Team;
