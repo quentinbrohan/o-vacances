bbbe6a99bf090cd7768b7dfd2532cf3863644098
QuentinBrohan <brohan.quentin@gmail.com>
2020-07-04 23:47:11 +0200

    :lipstick: Update Header max-w 1152px

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -6,23 +6,42 @@ import './header.scss';
 
 const Header = () => (
   <header>
-  <div className="header-container">
+    <div className="header-container">
 
-    <a href="/">LOGO</a>
-    <nav className="navigation">
-      <div className="mobile">
-        <Menu
-          right
-          noOverlay
-          className="bm-menu"
-          customBurgerIcon={<MenuIcon />}
-          customCrossIcon={<CloseIcon />}
-        >
-          <a href="">Mes voyages</a>
-          <a href="">Créer un voyage</a>
-          <a href="">Mon profil</a>
+      <a href="/">LOGO</a>
+      <nav className="navigation">
+        <div className="mobile">
+          <Menu
+            right
+            noOverlay
+            className="bm-menu"
+            customBurgerIcon={<MenuIcon />}
+            customCrossIcon={<CloseIcon />}
+          >
+            <a href="">Mes voyages</a>
+            <a href="">Créer un voyage</a>
+            <a href="">Mon profil</a>
 
-          <div className="connection-mobile">
+            <div className="connection-mobile">
+              <button type="button">
+                <a href="/">Connexion</a>
+              </button>
+              <button type="button">
+                <a href="/">Déconnexion</a>
+              </button>
+              <button type="button">
+                <a href="/">Inscription</a>
+              </button>
+            </div>
+          </Menu>
+        </div>
+        <div className="menu">
+          <ul>
+            <li><a href="">Mes voyages</a></li>
+            <li><a href="">Créer un voyage</a></li>
+            <li><a href="">Mon profil</a></li>
+          </ul>
+          <div className="connection">
             <button type="button">
               <a href="/">Connexion</a>
             </button>
@@ -33,28 +52,9 @@ const Header = () => (
               <a href="/">Inscription</a>
             </button>
           </div>
-        </Menu>
-      </div>
-      <div className="menu">
-        <ul>
-          <li><a href="">Mes voyages</a></li>
-          <li><a href="">Créer un voyage</a></li>
-          <li><a href="">Mon profil</a></li>
-        </ul>
-        <div className="connection">
-          <button type="button">
-            <a href="/">Connexion</a>
-          </button>
-          <button type="button">
-            <a href="/">Déconnexion</a>
-          </button>
-          <button type="button">
-            <a href="/">Inscription</a>
-          </button>
         </div>
-      </div>
-    </nav>
-  </div>
+      </nav>
+    </div>
   </header>
 );
 
