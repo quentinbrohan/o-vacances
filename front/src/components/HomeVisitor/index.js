9127310b19f96503d758d442f346c4af6e8ecfde
QuentinBrohan <brohan.quentin@gmail.com>
2020-07-05 02:50:08 +0200

    :construction: Add Slideshow w/o animations nor centred

--- front/src/components/HomeVisitor/index.js
+++ front/src/components/HomeVisitor/index.js
@@ -1,13 +1,70 @@
-import React from 'react';
+/* eslint-disable react/no-array-index-key */
+import React, { useState, useEffect, useRef } from 'react';
+import { ArrowDownCircle } from 'react-feather';
 
 import slideshowData from 'src/data/homeVisitorData';
+import './homeVisitor.scss';
 
 const HomeVisitor = () => {
-  console.log(slideshowData);
+  // console.log(slideshowData);
+  // Duration in ms for each slide
+  const time = 5000;
+  const [index, set] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => set((index + 1) % 2), time);
+    return () => clearInterval(interval);
+  }, [index]);
+
+  const ref = useRef();
+  // useEffect(() => {
+  //   ref.current.style.animation = 'none';
+  //   void ref.current.offsetHeight;
+  //   ref.current.style.animation = `changewidth ${time / 1000}s linear`;
+  // }, [index]);
 
   return (
     <div className="home-visitor">
-      Home
+      {/* Slideshow Images/Text */}
+      <div className="slideshow">
+        <div className="slideshow-images">
+          {slideshowData.map(({ image }, i) => (
+            <div
+              key={i}
+              hidden={i !== index || undefined}
+              style={{ backgroundImage: `url('${image}')` }}
+              className="slideshow-image"
+              alt="Voyage"
+            />
+          ))}
+        </div>
+        <div className="slideshow-content">
+          <p className="intro-title">Plannifer vos vacances{' '}
+            {slideshowData.map(({ name }, i) => (
+              <span
+                key={i}
+                hidden={i !== index || undefined}
+                className="trip-type"
+              >
+                {name}
+              </span>
+
+            ))}
+            {' '}
+            devient facile.
+          </p>
+          <p className="intro-content">Partir seul ou à plusieurs, gérer ses activités, échanger entre amis avec O'Vacances. <br />
+            Tomber amoureux du voyage à nouveau.
+          </p>
+        </div>
+      </div>
+
+      <button
+        className="show-features"
+        type="button"
+      >
+        Voir les fonctionnalités <ArrowDownCircle />
+      </button>
+
     </div>
   );
 };
