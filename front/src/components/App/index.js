c63900535e400df750f902e1fef7620b1bca7ef8
QuentinBrohan <brohan.quentin@gmail.com>
2020-07-06 13:08:32 +0200

    :lipstick: Fix Footer always bottom w/ page height min-100%

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -1,5 +1,9 @@
 // == Import npm
 import React from 'react';
+import {
+  Switch,
+  Route,
+} from 'react-router-dom';
 
 // == Import
 import Header from 'src/components/Header';
@@ -13,7 +17,13 @@ const App = () => (
   <div className="app">
     <Header />
     <div className="container">
-      <HomeVisitor />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={HomeVisitor}
+        />
+      </Switch>
     </div>
     <Footer />
   </div>
