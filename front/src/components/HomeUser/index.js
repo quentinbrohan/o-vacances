6452779a524c2edb7c0174f68dd8fc8befa4c86f
QuentinBrohan <brohan.quentin@gmail.com>
2020-07-07 18:02:59 +0200

    :art: Add HomeVisitor old trips w/ fake data

--- front/src/components/HomeUser/index.js
+++ front/src/components/HomeUser/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import userTrips from 'src/data/homeUserTripsData';
+import userOldTrips from 'src/data/homeUserOldTripsData';
 import TripCard from './TripCard';
 
 import './homeUser.scss';
@@ -12,6 +13,13 @@ const HomeUser = () => {
       <h1>Mes Voyages</h1>
       <div className="my-trips">
         {userTrips.map((trip) => (
+          <TripCard {...trip} key={trip.id} />
+        ))}
+      </div>
+
+      <h2>Mes anciens voyages</h2>
+      <div className="my-trips">
+        {userOldTrips.map((trip) => (
           <TripCard {...trip} key={trip.id} />
         ))}
       </div>
