f884229575d469fbf0aea09a52aff62796219fa5
Audrey-Chaigne <chaigne.audrey@gmail.com>
2020-07-07 16:33:46 +0200

    :sparkles: api trip

--- back/src/Controller/TripController.php
+++ back/src/Controller/TripController.php
@@ -2,18 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\TripRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class TripController extends AbstractController
 {
     /**
-     * @Route("/trip", name="trip")
+     * @Route("/api/v0/trips?user_id={id}", name="api_v0_trips_list", methods="GET")
      */
-    public function index()
+    public function list(TripRepository $tripRepository, ObjectNormalizer $normalizer)
     {
-        return $this->render('trip/index.html.twig', [
-            'controller_name' => 'TripController',
-        ]);
+    
+        $trips = $tripRepository->findAll();
+
+        // On instancie un serializer en lui précisant un normalizer adapté aux objets PHP
+        $serializer = new Serializer([$normalizer]);
+        // Parce qu'on a précisé le normalizer, on peut normaliser selon un groupe
+        $normalizedTrips = $serializer->normalize($trips, null, ['groups' => 'apiV0']);
+
+        // dd($normalizedAnimes);
+
+        return $this->json($normalizedTrips);
+        
+        
     }
 }
