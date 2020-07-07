27b5c41f103adbee12639a532d2479abf4759b5d
CeciliaCavallini <caval.ceci@gmail.com>
2020-07-07 16:56:40 +0200

    :lipstick: add css

--- front/src/components/Profile/ProfileField/ProfileField.js
+++ front/src/components/Profile/ProfileField/ProfileField.js
@@ -1,12 +1,17 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+
+import { Edit2 } from 'react-feather';
 
 import './profilefield.scss';
 
 // component to contact form
 const ProfileField = () => (
+
   <div className="profile-field">
+    <label htmlFor="button">Name</label>
     <input type="text" />
-    <button type="button">Pencil</button>
+    <Edit2 className="pen" />
   </div>
 );
 
--- front/src/components/Profile/index.js
+++ front/src/components/Profile/index.js
@@ -8,10 +8,11 @@ import './profile.scss';
 const Profile = () => (
 
   <div className="profile">
+    <h1>Mon profil</h1>
     <div className="profile-head">
       <div className="profile-head-img">
         <img className="profile-head-img-picture" src="https://via.placeholder.com/100x100" alt="" />
-        <button type="button">Modifier la photo de profil</button>
+        <button type="button">Modifier la photo</button>
       </div>
       <div className="profile-head-information">
         <ProfileField />
@@ -20,11 +21,17 @@ const Profile = () => (
         <ProfileField />
       </div>
     </div>
+    <hr />
     <div className="profile-trip">
+      <h2>J'organise un voyage</h2>
       <div className="profile-trip-information">
-        <p>Fonctionnalités</p>
-        <button type="button">Créer un voyage</button>
+        <p>Je créé un voygage. J'invite mes amis ou ma famille. On s'organise ensemble avec O'Vacances!
+          <br />
+          Agenda, lieux, activités, suggestions,... 
+        </p>
+        <button type="button">GO</button>
       </div>
+      <hr />
       <div className="profile-trip-archive">
         Résumés des anciens voyages
       </div>
