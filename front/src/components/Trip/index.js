b71827531d4d16defa0e2473fbc308436630f7bf
QuentinBrohan <brohan.quentin@gmail.com>
2020-07-08 11:28:25 +0200

    :init: Creating Trip page

---
+++ front/src/components/Trip/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './trip.scss';
+
+const Trip = () => {
+  console.log('Trip component');
+
+  return (
+    <main className="trip-details">
+
+    </main>
+  );
+};
+
+export default Trip;
