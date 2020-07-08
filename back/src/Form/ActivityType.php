eb221b23fe969a860eb462c05fa9961bf9fa3d53
Etudiant O'clock <etudiant@teleporteur.oclock>
2020-07-08 16:03:11 +0200

    :sparkles:-api-activity

---
+++ back/src/Form/ActivityType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Activity;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ActivityType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('start_date')
+            ->add('end_date')
+            ->add('creator')
+            ->add('category')
+            ->add('trip')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Activity::class,
+        ]);
+    }
+}
