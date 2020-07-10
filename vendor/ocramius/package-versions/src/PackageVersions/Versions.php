37cea001712a3f9a80023ae029cfe293b174db06
Audrey-Chaigne <chaigne.audrey@gmail.com>
2020-07-10 18:35:58 +0200

    -

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -68,7 +68,7 @@ final class Versions
   'symfony/string' => 'v5.1.2@ac70459db781108db7c6d8981dd31ce0e29e3298',
   'symfony/var-dumper' => 'v5.1.2@46a942903059b0b05e601f00eb64179e05578c0f',
   'symfony/var-exporter' => 'v5.1.2@eabaabfe1485ca955c5b53307eade15ccda57a15',
-  '__root__' => 'dev-back@53201a94282f2604f018b6f6113437ec53563cdf',
+  '__root__' => 'dev-master@1976955a9f0276991b27765e425f66fc681866d7',
 );
 
     private function __construct()
