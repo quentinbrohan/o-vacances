9316928d1316d7d2dc6f99d953a30862b15cc568
QuentinBrohan <brohan.quentin@gmail.com>
2020-07-16 23:36:25 +0200

    :lipstick: :sparkles: add Loading spinner

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -1,5 +1,5 @@
 // == Import npm
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import {
   Switch,
   Route,
@@ -26,6 +26,7 @@ import TripForm from 'src/containers/TripForm';
 import TripEdit from 'src/containers/TripEdit';
 import ActivityForm from 'src/components/ActivityForm';
 import LegacyMentions from 'src/components/LegacyMentions';
+import Loading from 'src/components/Loading';
 
 // Data
 import persons from 'src/data/teamData';
@@ -45,81 +46,83 @@ const App = ({
     <div className="app">
       <Header />
       <div className="container">
-        <Switch>
-          {isAuthenticated
-            ? (
-              <Route
-                exact
-                path="/"
-                component={HomeUser}
-              />
-            )
-            : (
-              <Route
-                exact
-                path="/"
-                component={HomeVisitor}
-              />
-            )}
-          <Route
-            path="/contact"
-            component={Contact}
-          />
-          <Route
-            path="/qui-sommes-nous"
-            render={() => <Team persons={persons} />}
-          />
-          <Route
-            path="/mentions-legales"
-            component={LegacyMentions}
-          />
-          <Route
-            path="/signin"
-            component={Signin}
-          />
-          <Route
-            path="/login"
-            component={Login}
-          />
-          <PrivateRoute
-            exact
-            path="/mon-profil"
-            component={Profile}
-            isAuthenticated={isAuthenticated}
-          />
-          <PrivateRoute
-            exact
-            path="/voyage/:id"
-            component={Trip}
-            isAuthenticated={isAuthenticated}
-          />
-          <PrivateRoute
-            exact
-            path="/voyage/:id/activites"
-            component={Activities}
-            isAuthenticated={isAuthenticated}
-          />
-          <PrivateRoute
-            path="/mes-voyages"
-            component={HomeUser}
-            isAuthenticated={isAuthenticated}
-          />
-          <PrivateRoute
-            path="/creer-un-voyage"
-            component={TripForm}
-            isAuthenticated={isAuthenticated}
-          />
-          <PrivateRoute
-            path="/modifier-un-voyage"
-            component={TripEdit}
-            isAuthenticated={isAuthenticated}
-          />
-          <Route
-            path="/ajouter-une-activite"
-            component={ActivityForm}
-          />
-          <Route component={ErrorPage} />
-        </Switch>
+        <Suspense fallback={<Loading />}>
+          <Switch>
+            {isAuthenticated
+              ? (
+                <Route
+                  exact
+                  path="/"
+                  component={HomeUser}
+                />
+              )
+              : (
+                <Route
+                  exact
+                  path="/"
+                  component={HomeVisitor}
+                />
+              )}
+            <Route
+              path="/contact"
+              component={Contact}
+            />
+            <Route
+              path="/qui-sommes-nous"
+              render={() => <Team persons={persons} />}
+            />
+            <Route
+              path="/mentions-legales"
+              component={LegacyMentions}
+            />
+            <Route
+              path="/signin"
+              component={Signin}
+            />
+            <Route
+              path="/login"
+              component={Login}
+            />
+            <PrivateRoute
+              exact
+              path="/mon-profil"
+              component={Profile}
+              isAuthenticated={isAuthenticated}
+            />
+            <PrivateRoute
+              exact
+              path="/voyage/:id"
+              component={Trip}
+              isAuthenticated={isAuthenticated}
+            />
+            <PrivateRoute
+              exact
+              path="/voyage/:id/activites"
+              component={Activities}
+              isAuthenticated={isAuthenticated}
+            />
+            <PrivateRoute
+              path="/mes-voyages"
+              component={HomeUser}
+              isAuthenticated={isAuthenticated}
+            />
+            <PrivateRoute
+              path="/creer-un-voyage"
+              component={TripForm}
+              isAuthenticated={isAuthenticated}
+            />
+            <PrivateRoute
+              path="/modifier-un-voyage"
+              component={TripEdit}
+              isAuthenticated={isAuthenticated}
+            />
+            <Route
+              path="/ajouter-une-activite"
+              component={ActivityForm}
+            />
+            <Route component={ErrorPage} />
+          </Switch>
+        </Suspense>
       </div>
       <Footer />
     </div>
