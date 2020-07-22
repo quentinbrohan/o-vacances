e50c53354c55724362086237aa62536ab559b8ea
Audrey-Chaigne <chaigne.audrey@gmail.com>
2020-07-22 16:53:50 +0200

    :pencil2:

--- back/src/Entity/User.php
+++ back/src/Entity/User.php
@@ -52,7 +52,7 @@ class User implements UserInterface
 
     /**
      * @var string The hashed password
-     * @ORM\Column(type="string", nullable=true)
+     * @ORM\Column(type="string")
      * 
      */
     private $password;
